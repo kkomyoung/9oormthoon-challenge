8ef3b3850af5cd32846f0164621594c0a5783641
kkomyoung <kya754@gmail.com>
2023-09-03 07:05:45 +0000

    폴더명 변경

--- Week1/Day1/solution.py
+++
@@ -1,3 +0,0 @@
-W, R = map(int, input().split())
-RM = W*(1+R/30)
-print(int(RM))
--- Week1/Day3/solution.py
+++
@@ -1,9 +0,0 @@
-user_input = int(input())
-
-result = 0
-for i in range(user_input):
-    inputValue = input()
-    result += int(eval(inputValue))
-    
-print(result)
-
